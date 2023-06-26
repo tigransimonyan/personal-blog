bcda8d01e5e273e42ddbb9b9bbf33f1c999ca3ec
tigran <tigran.contact@gmail.com>
2023-06-27 02:25:06 +0400

    update

--- assets/js/_commons/image-viewer.js
+++ assets/js/_commons/image-viewer.js
@@ -1,10 +1,15 @@
 $(function () {
-  var elem = document.getElementById('post-wrapper');
+  var elem = document.getElementById('main');
   if (elem && Viewer) {
     new Viewer(elem, {
       movable: false,
       transition: false,
       toggleOnDblclick: false,
+      rotatable: false,
+      navbar: false,
+      scalable: false,
+      zoomable: false,
+      tooltip: false,
     });
   }
 });
